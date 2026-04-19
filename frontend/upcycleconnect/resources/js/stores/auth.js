ac677745827136d7aa4eee44cc8f80bed8fbc129
ber <reynaudberenger1@gmail.com>
2026-04-19 13:02:48 +0200

    passe de local storage à session storage

--- frontend/upcycleconnect/resources/js/stores/auth.js
+++ frontend/upcycleconnect/resources/js/stores/auth.js
@@ -3,8 +3,8 @@ import { ref, computed } from "vue";
 import api, { setAuthToken, clearAuthToken } from "../api";
 
 export const useAuthStore = defineStore("auth", () => {
-    const token = ref(localStorage.getItem("token") ?? null);
-    const user = ref(JSON.parse(localStorage.getItem("user") ?? "null"));
+    const token = ref(sessionStorage.getItem("token") ?? null);
+    const user = ref(JSON.parse(sessionStorage.getItem("user") ?? "null"));
 
     const isLoggedIn = computed(() => !!token.value);
     const isAdmin = computed(
@@ -17,8 +17,8 @@ export const useAuthStore = defineStore("auth", () => {
         token.value = data.token
         user.value = data.user
 
-        localStorage.setItem('token', data.token)
-        localStorage.setItem('user', JSON.stringify(data.user))
+        sessionStorage.setItem('token', data.token)
+        sessionStorage.setItem('user', JSON.stringify(data.user))
 
         setAuthToken(data.token)
     }
@@ -26,12 +26,14 @@ export const useAuthStore = defineStore("auth", () => {
     function logout() {
         token.value = null
         user.value = null
-        localStorage.removeItem('token')
-        localStorage.removeItem('user')
+        sessionStorage.removeItem('token')
+        sessionStorage.removeItem('user')
         clearAuthToken()
     }
 
     function init(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
         if(token.value){
             setAuthToken(token.value)
         }
