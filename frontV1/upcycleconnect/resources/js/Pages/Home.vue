d0b9f8dd309c28804f52437aa838ed7c66fa92f2
REYNAUD-Berenger <reynaudberenger1@gmail.com>
2026-06-29 15:18:02 +0200

    Delete frontV1 directory
    nettoyage du main

--- frontV1/upcycleconnect/resources/js/Pages/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="min-h-screen flex items-center justify-center" style="background-color: #EFE7DA">
-    <h1 class="text-4xl font-bold" style="color: #c46d68">
-      Bienvenue sur UpcycleConnect
-    </h1>
-  </div>
-</template>
